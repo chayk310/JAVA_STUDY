cf024bedf4ae6e71dc292da905ad84c2b05e82fa
admin <admin@10.137.138.102>
2019-01-10 18:13:49 +0900

    homework

---
+++ src/icehs/science/chater04/SalaryTest.java
@@ -0,0 +1,10 @@
+package icehs.science.chater04;
+
+public class SalaryTest {
+
+	public static void main(String[] args) {
+		// 66p
+
+	}
+
+}
---
+++ src/icehs/science/chater04/SeperateNumberTest.java
@@ -0,0 +1,9 @@
+package icehs.science.chater04;
+
+public class SeperateNumberTest {
+
+	public static void main(String[] args) {
+		//66p
+	}
+
+}
