1a97a5f242eb83734542bec8578c9f82f468d842
admin <admin@10.137.138.102>
2019-01-11 14:06:43 +0900

    homework

--- src/icehs/science/chater04/SalaryTest.java
+++ src/icehs/science/chater04/SalaryTest.java
@@ -3,7 +3,19 @@ package icehs.science.chater04;
 public class SalaryTest {
 
 	public static void main(String[] args) {
-		// 66p
+		long salary = 1000000L;
+		long bonus = ( salary / 5 ) * 4;
+		
+		salary *= 12; //연봉
+		
+		long salaryTax = salary/10;
+		double bonusTax = bonus * 0.055;
+		long longBonusTax = (long) bonusTax ;
+
+
+		System.out.println("연봉 : " + salary + ", 세후 연봉 : " + ( salary - salaryTax ));
+		System.out.println("보너스 : " + bonus + ", 세후 보너스 : " + ( bonus - longBonusTax ));
+		System.out.println("지급액 : " + (salary - salaryTax + bonus - longBonusTax ) );
 
 	}
 
--- src/icehs/science/chater04/SeperateNumberTest.java
+++ src/icehs/science/chater04/SeperateNumberTest.java
@@ -3,7 +3,14 @@ package icehs.science.chater04;
 public class SeperateNumberTest {
 
 	public static void main(String[] args) {
-		//66p
+		int num = 456;
+
+		System.out.println("숫자 : " + num );
+		System.out.println("백의 자리 수 : " + num/100 );
+		num = num % 100;
+		System.out.println("십의 자리 수 : " + num/10 );
+		num = num % 10;
+		System.out.println("일의 자리 수 : " + num );
 	}
 
 }
