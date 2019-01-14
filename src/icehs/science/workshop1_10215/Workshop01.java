de865f3eae467488c55bd77e9e0178256eea2d09
admin <admin@10.137.138.102>
2019-01-14 18:27:50 +0900

    homework

---
+++ src/icehs/science/workshop1_10215/Workshop01.java
@@ -0,0 +1,14 @@
+package icehs.science.workshop1_10215;
+
+//문제 1.
+//본인의 주민등록번호를 정수형 변수에 선언하고, 이를 화면에 출력하시오.
+public class Workshop01 {
+ 
+ public static void main( String[] args ) {
+	 
+     int frontNum = 920310;
+     long rearNum = 1234567L;
+     
+     System.out.println("주민등록번호 : " + frontNum + " - " + rearNum );
+ }
+}
